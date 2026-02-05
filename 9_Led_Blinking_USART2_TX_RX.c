ef237bd51c393f28202edb429303dea5a00b85ba
foxer04 <alexfucile04@gmail.com>
2026-02-05 10:40:36 +0100

    Versione definitiva del programma, il cui codice risulta meno da Antonio. I file sono stati ridotti da 4 a 2

--- 9_Led_Blinking_USART2_TX_RX.c
+++ 9_Led_Blinking_USART2_TX_RX.c
@@ -1,91 +1,118 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include "STM32F413xx.h" // Header corretto
-#include "functions.h"
+#include "STM32F413xx.h"
 
-// Variabili di stato
-uint32_t current_cpu_freq = 16;
+long current_cpu_freq = 16;
 char menu_buffer[100];
 
-// Prototipi locali
+//prototipi
+void init(void);
+void scrivi_char(char c);
+void scrivi_stringa(char* str);
+void set_clock_100MHz(void);
+void set_clock_16MHz(void);
 void stampa_intestazione(void);
 void gestisci_menu_frequenza(void);
+void funzione_can_transmit_sim(void);
+void funzione_can_receive_sim(void);
 
-// Importa funzione specifica F413
-extern void set_clock_100MHz(void);
+int main(void) {
+    init(); 
+    scrivi_stringa("\n\r** STM32F413 PRONTA **\n\r");
 
-int main(void)
-{
-	char scelta;
-	init(); // Partenza a 16 MHz
-	scrivi_stringa("\n\r\n\r** STM32F413 BOOT SUCCESSFUL **\n\r");
-	while(1)
-		{
-			stampa_intestazione();
+    while(1) {
+        stampa_intestazione();
+        while(!(USART1_SR & 0x00000020)); 
+        char scelta = (char)USART1_DR;
 
-			while(!(USART1_SR & 0x00000020)); // Attesa RX
-				scelta = (char)USART1_DR;
-				switch(scelta)
-					{
-						case '1':
-						scrivi_stringa("\n\r[CMD] Configurazione Clock...\n\r");
-						gestisci_menu_frequenza();
-						break;
-						case '2':
-						funzione_can_transmit_sim();
-						break;
-						case '3':
-						funzione_can_receive_sim();
-						break;
-						default:
-						scrivi_stringa("\n\r[ERR] Comando non valido.\n\r");
-						break;
-					}
-				}
+        switch(scelta) {
+            case '1': gestisci_menu_frequenza(); break;
+            case '2': funzione_can_transmit_sim(); break;
+            case '3': funzione_can_receive_sim(); break;
+            default:  scrivi_stringa("\n\r[ERR] Comando errato.\n\r"); break;
+        }
+    }
 }
 
-void stampa_intestazione(void)
-{
-	scrivi_stringa("\n\r--- F413 DASHBOARD ---\n\r");
-	sprintf(menu_buffer, "Clock attuale: %d MHz\n\r", (int)current_cpu_freq);
-	scrivi_stringa(menu_buffer);
-	scrivi_stringa("1. Modifica Frequenza (16/100 MHz)\n\r");
-	scrivi_stringa("2. Test CAN TX\n\r");
-	scrivi_stringa("3. Test CAN RX\n\r");
-	scrivi_stringa("Inserisci comando > ");
-}
 
-void gestisci_menu_frequenza(void)
-{
- char sub_scelta;
- scrivi_stringa("\n\r--- SELEZIONE PERFORMANCE ---\n\r");
- scrivi_stringa("a. Modalita' Low Power (16 MHz)\n\r");
- scrivi_stringa("b. Modalita' Turbo F413 (100 MHz)\n\r");
- scrivi_stringa("> ");
+void init(void) {
 	
- while(!(USART1_SR & 0x00000020));
-		sub_scelta = (char)USART1_DR;
-		if(sub_scelta == 'a') {
-			if(current_cpu_freq == 16) {
-				scrivi_stringa("\n\r[INFO] Gia' in Low Power.\n\r");
- } else {
-				scrivi_stringa("\n\r[SYS] Riduzione clock a 16 MHz...\n\r");
-				set_clock_16MHz();
-				current_cpu_freq = 16;
-				scrivi_stringa("[OK] Clock ridotto.\n\r");
-				}
-} else if(sub_scelta == 'b') {
-				if(current_cpu_freq == 100) {
-				scrivi_stringa("\n\r[INFO] Gia' alla massima velocita'.\n\r");
- } else {
-				scrivi_stringa("\n\r[SYS] Spinta PLL a 100 MHz (3WS Flash)...\n\r");
-				set_clock_100MHz(); // Funzione specifica aggiornata
-				current_cpu_freq = 100;
-				scrivi_stringa("[OK] Sistema operativo a 100 MHz.\n\r");
-				}
- } else {
-				scrivi_stringa("\n\r[ERR] Scelta errata.\n\r");
-				}
+    RCC_AHB1ENR = (long)0x00000001; 
+    RCC_APB1ENR = (long)0x10000000; 
+    RCC_APB2ENR = (long)0x00000010; 
+
+    //scale 1
+    PWR_CR = (long)0x0000C000; 
+
+    GPIOA_MODER = (long)0xA8280400;  
+
+    GPIOA_PUPDR = (long)0x00140000; 
+
+    GPIOA_AFRH  = (long)0x00000770;  
+
+	//USART1: 9600 brr, 16 MHz
+    USART1_BRR = (long)0x00000683;  
+    USART1_CR1 = (long)0x0000200C; 
 }
 
+void scrivi_char(char c) {
+    while(!(USART1_SR & 0x00000080)); 
+    USART1_DR = c;
+}
+
+void scrivi_stringa(char* str) {
+    while(*str) scrivi_char(*str++);
+}
+
+void set_clock_100MHz(void) {
+    USART1_CR1 = (long)0x00000000; 
+    FLASH_ACR = (long)0x00000704; 
+
+    RCC_PLLCFGR = (long)0x02002810; 
+    RCC_CR = (long)0x01000081; 
+    while(!(RCC_CR & 0x02000000)); 
+
+    RCC_CFGR = (long)0x00001002; 
+    while ((RCC_CFGR & 0x0000000C) != 0x00000008);
+
+    for(int i = 0; i < 200000; i++); 
+			USART1_BRR = (long)0x0000208D; 
+			USART1_CR1 = (long)0x0000200C;
+}
+
+void set_clock_16MHz(void) {
+    USART1_CR1 = (long)0x00000000;
+    RCC_CFGR = (long)0x00000000; 
+    while ((RCC_CFGR & 0x0000000C) != 0x00000000);
+
+    RCC_CR = (long)0x00000081; 
+    FLASH_ACR = (long)0x00000000;
+
+    USART1_BRR = (long)0x00000683; 
+    USART1_CR1 = (long)0x0000200C;
+}
+
+void stampa_intestazione(void) {
+    scrivi_stringa("\n\r--- F413 DASHBOARD ---\n\r");
+    sprintf(menu_buffer, "Clock: %ld MHz\n\r", current_cpu_freq);
+    scrivi_stringa(menu_buffer);
+    scrivi_stringa("1. Clock | 2. CAN TX | 3. CAN RX\n\r> ");
+}
+
+void gestisci_menu_frequenza(void) {
+    scrivi_stringa("\n\r(a) 16MHz (b) 100MHz > ");
+    while(!(USART1_SR & 0x00000020));
+    char sub = (char)USART1_DR;
+
+    if(sub == 'a' && current_cpu_freq != 16) {
+        set_clock_16MHz(); current_cpu_freq = 16;
+        scrivi_stringa("\n\r[OK] 16 MHz.\n\r");
+    } else if(sub == 'b' && current_cpu_freq != 100) {
+        set_clock_100MHz(); current_cpu_freq = 100;
+        scrivi_stringa("\n\r[OK] 100 MHz.\n\r");
+    }
+}
+
+void funzione_can_transmit_sim(void) { scrivi_stringa("\n\r[CAN] TX Sim...\n\r"); }
+void funzione_can_receive_sim(void) { scrivi_stringa("\n\r[CAN] RX Sim...\n\r"); }
--- STM32F413XX.h
+++ STM32F413XX.h
@@ -1,38 +1,26 @@
-#ifndef STM32F413XX_H //all'evenienza, non viene caricato due volte lo stesso documento
+#ifndef STM32F413XX_H
 #define STM32F413XX_H
+
 #include <stdint.h>
 
-//clock enable
-#define RCC_AHB1ENR   *(long*)0x40023830 
-#define RCC_APB1ENR   *(long*)0x40023840
-#define RCC_APB2ENR   *(long*)0x40023844 
+#define RCC_CR         *(long*)0x40023800 
+#define RCC_PLLCFGR    *(long*)0x40023804 
+#define RCC_CFGR       *(long*)0x40023808 
+#define RCC_AHB1ENR    *(long*)0x40023830 
+#define RCC_APB1ENR    *(long*)0x40023840 
+#define RCC_APB2ENR    *(long*)0x40023844 
 
-//porta A
-#define GPIOA_MODER   *(long*)0x40020000 
-#define GPIOA_OTYPER  *(long*)0x40020004	
-#define GPIOA_OSPEEDR *(long*)0x40020008	
-#define GPIOA_PUPDR   *(long*)0x4002000C			
-#define GPIOA_ODR     *(long*)0x40020014 
-#define GPIOA_AFRL    *(long*)0x40020020 
-#define GPIOA_AFRH    *(long*)0x40020024 
+#define GPIOA_MODER    *(long*)0x40020000 
+#define GPIOA_PUPDR    *(long*)0x4002000C 
+#define GPIOA_AFRH     *(long*)0x40020024 
 
-//boundary USART1: 0x40011000
-#define USART1_SR     *(long*)0x40011000 
-#define USART1_DR     *(long*)0x40011004 
-#define USART1_BRR    *(long*)0x40011008 
-#define USART1_CR1    *(long*)0x4001100C
-#define USART1_CR2    *(long*)0x40011010
-#define USART1_CR3    *(long*)0x40011014
+#define USART1_SR      *(long*)0x40011000 
+#define USART1_DR      *(long*)0x40011004 
+#define USART1_BRR     *(long*)0x40011008 
+#define USART1_CR1     *(long*)0x4001100C 
 
-//boundary: 0x40023800
-#define RCC_CR        *(long*)0x40023800 
-#define RCC_PLLCFGR   *(long*)0x40023804 
-#define RCC_CFGR      *(long*)0x40023808 
-
-// FLASH Interface Base: 0x40023C00
-#define FLASH_ACR     *(long*)0x40023C00 
-
-// FPU Control
-#define CPACR         *(long*)0xE000ED88
+#define FLASH_ACR      *(long*)0x40023C00 
+#define PWR_CR         *(long*)0x40007000
+#define CPACR          *(long*)0xE000ED88
 
 #endif
